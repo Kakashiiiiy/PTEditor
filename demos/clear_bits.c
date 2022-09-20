cca3509e5e16b9ed022117eeaf9ae7114a806c28
Benedict Schlueter <benedictschlueter@gmail.com>
2022-09-20 09:17:26 +0200

    wip

--- demos/clear_bits.c
+++ demos/clear_bits.c
@@ -9,33 +9,45 @@
 
 #include "../ptedit_header.h"
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-  if (ptedit_init()) {
-      printf("Error: Could not initalize PTEditor, did you load the kernel module?\n");
-      return 1;
+  if (ptedit_init())
+  {
+    printf("Error: Could not initalize PTEditor, did you load the kernel module?\n");
+    return 1;
   }
-  
-  unsigned char* addr = (unsigned char*) mmap(NULL,2*1024*1024, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-  memset(addr,0x42,2*1024*1024);
-  
-  ptedit_entry_t entry = ptedit_resolve(addr, 0);
-  ptedit_clear_bit(addr, 0, PTEDIT_PAGE_BIT_ACCESSED,PTEDIT_VALID_MASK_PUD|PTEDIT_VALID_MASK_PMD|PTEDIT_VALID_MASK_PTE);
-  entry = ptedit_resolve(addr, 0);
-  ptedit_print_entry(entry.pte);
-  ptedit_print_entry(entry.pmd);
-  ptedit_print_entry(entry.pud);
-  ptedit_set_bit(addr, 0, PTEDIT_PAGE_BIT_ACCESSED,PTEDIT_VALID_MASK_PUD|PTEDIT_VALID_MASK_PMD|PTEDIT_VALID_MASK_PTE);
-  entry = ptedit_resolve(addr, 0);
-  ptedit_print_entry(entry.pte);
-  ptedit_print_entry(entry.pmd);
-  ptedit_print_entry(entry.pud);
 
-  //clear accessed bit of pd entry
+  unsigned char *addr = (unsigned char *)mmap(NULL, 2 * 1024 * 1024, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+  memset(addr, 0x42, 2 * 1024 * 1024);
+  addr[9] = '\n';
+  addr[10] = 0;
+
+  ptedit_entry_t entry = ptedit_resolve(addr, 0);
+/*   ptedit_clear_bit(addr, 0, PTEDIT_PAGE_BIT_ACCESSED, PTEDIT_VALID_MASK_PUD | PTEDIT_VALID_MASK_PMD | PTEDIT_VALID_MASK_PTE);
+  entry = ptedit_resolve(addr, 0); */
+  ptedit_print_entry(entry.pte);
+  ptedit_print_entry(entry.pmd);
+  ptedit_print_entry(entry.pud);
+/*   ptedit_set_bit(addr, 0, PTEDIT_PAGE_BIT_ACCESSED, PTEDIT_VALID_MASK_PUD | PTEDIT_VALID_MASK_PMD | PTEDIT_VALID_MASK_PTE);
+  entry = ptedit_resolve(addr, 0);
+  ptedit_print_entry(entry.pte);
+  ptedit_print_entry(entry.pmd);
+  ptedit_print_entry(entry.pud); */
+  // ptedit_print_entry(entry.pgd);
+  printf("%llx\n", entry.pmd);
+  ptedit_entry_t vm = ptedit_resolve(addr, 0);
+  vm.pmd = (entry.pmd & 0xfff) | 0x94000000;
+  vm.valid = PTEDIT_VALID_MASK_PMD;
+  ptedit_update(addr, 0, &vm);
+  entry = ptedit_resolve(addr, 0);
+  printf("%llx\n", entry.pmd);
+
+  printf("%s", addr);
+
+/*   // clear accessed bit of pd entry
   size_t address_pfn = ptedit_get_pfn(entry.pd);
-  ptedit_set_bit(addr, 0, PTEDIT_PAGE_BIT_ACCESSED,PTEDIT_VALID_MASK_PMD);
-  munmap(addr,4096);
+  ptedit_set_bit(addr, 0, PTEDIT_PAGE_BIT_ACCESSED, PTEDIT_VALID_MASK_PMD); */
+  munmap(addr, 4096);
 
   ptedit_cleanup();
 
